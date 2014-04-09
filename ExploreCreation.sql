5dd445831cd2d28e65ae7d7b4a1aaf1ba2859afa
Bluma Gelley <bsgelley@gmail.com>
2014-04-09 17:47:17 -0400

    bad old query and beginning of new fixed one

--- ExploreCreation.sql
+++ ExploreCreation.sql
@@ -19,7 +19,21 @@ USING (`user_id`) INNER JOIN nov13_page_origin USING (`page_id`) WHERE nov13_rev
 CREATE TABLE fastcreations SELECT creation.rev_id AS rev_id, creation.page_id AS page_id, creation.rev_comment AS rev_comment, creation.user_id AS user_id, 
 creation.user_text AS username, creation.rev_timestamp AS rev_timestamp, creation.rev_len AS rev_len, users.user_registration AS registration_timestamp, 
 users.account_creation_action AS creation_action, page.page_namespace AS namespace, page.page_title AS page_title,
-page.original_namespace AS orig_namespace, page.original_title AS orig_title, enwiki_p.logging.log__type AS log_type, enwiki_p.logging.log_action AS log_action, enwiki_p.logging.log_timestamp AS log_timestamp, 
-enwiki_p.logging.log_comment AS log_comment FROM nov13_creation AS creation INNER JOIN nov13_user_stats AS users USING (user_id)
+page.original_namespace AS orig_namespace, page.original_title AS orig_title, logging.log_type AS log_type, logging.log_action AS log_action, logging.log_timestamp AS log_timestamp, 
+logging.log_comment AS log_comment FROM nov13_creation AS creation INNER JOIN nov13_user_stats AS users USING (user_id)
 INNER JOIN nov13_page_origin AS page USING (page_id) LEFT OUTER JOIN enwiki_p.archive AS archive ON creation.rev_id = archive.ar_rev_id INNER JOIN 
 enwiki_p.logging AS logging ON creation.page_id = logging.log_page WHERE creation.rev_timestamp > 20090101000000  AND creation.rev_timestamp <= DATE_ADD(users.user_registration, INTERVAL 30 DAY) AND (page.page_namespace = 0 OR page.original_namespace = 0);
+
+mysql --defaults-file=~/replica.my.cnf -A -h enwiki.labsdb u4817__afcresearch -e CREATE TABLE `nov13_page` (
+  `page_id` int(10) unsigned DEFAULT NULL,
+  `page_namespace` int(11) NOT NULL DEFAULT '0',
+  `page_title` varbinary(255) NOT NULL DEFAULT '',
+  `revisions` bigint(21) NOT NULL DEFAULT '0',
+  `first_revision` varbinary(14) DEFAULT NULL,
+  `last_revision` varbinary(14) DEFAULT NULL,
+  `archived` int(1) NOT NULL DEFAULT '0',
+  `first_rev_id` int(8) unsigned DEFAULT NULL,
+  UNIQUE KEY `page_name_title` (`page_id`,`page_namespace`,`page_title`),
+  KEY `first_revision_idx` (`first_revision`),
+  KEY `namespace_title` (`page_namespace`,`page_title`)
+  
